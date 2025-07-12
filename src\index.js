70c5a00374a6dc75c5ac2e026eb444f60d72cb5d
Rjeronimo1 <roberto.jeronimo@hotmail.com>
2025-07-12 19:35:09 +0100

    [ATOM] Alteração: src\index.js

---
+++ src\index.js
@@ -0,0 +1,5 @@
+// index.js — Entrada principal do ATOM para ambientes Electron/Node
+// Encaminha para o núcleo real do sistema
+// Atualizado para suportar inicialização limpa e modular
+
+import "./mainPainel.js";
