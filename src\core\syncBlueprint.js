54790918a707466c26cee54c1ed2e5b0d502641e
Rjeronimo1 <roberto.jeronimo@hotmail.com>
2025-07-12 23:00:04 +0100

    [ATOM] Alteração: src\core\syncBlueprint.js

--- src\core\syncBlueprint.js
+++ src\core\syncBlueprint.js
@@ -1,5 +1,8 @@
-// src/core/syncBlueprint.js
+import fs from 'fs/promises';
 import { commitBlueprintSequencial } from './codexGitHubBridge.mjs';
-import blueprint from '../../atom_blueprint.json' assert { type: 'json' };
+
+// Lê o JSON diretamente do arquivo
+const blueprintData = await fs.readFile('../../atom_blueprint.json', 'utf-8');
+const blueprint = JSON.parse(blueprintData);
 
 await commitBlueprintSequencial(blueprint);
